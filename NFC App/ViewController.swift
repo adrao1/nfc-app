f9b7687ff9e460127af3b906d26aa0a8d164531d
Aditya Rao <adrao@berkeley.edu>
2024-11-12 11:29:09 -0800

    Resetting code

--- NFC App/ViewController.swift
+++ NFC App/ViewController.swift
@@ -6,64 +6,14 @@
 //
 
 import UIKit
-import CoreNFC
 
-class ViewController: UIViewController, NFCTagReaderSessionDelegate {
-    var session: NFCTagReaderSession?
-    
-    func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
-        print("Session became active. Ready to scan for tags.")
-    }
-    
-    func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: any Error) {
-        print("Session invalidated: \(error.localizedDescription)")
-        self.session = nil
-    }
-    
-    func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
-        guard let tag = tags.first else {
-            print("No tags detected.")
-            return
-        }
-        
-        session.connect(to: tag) { (error) in
-            if let error = error {
-                print("Error connecting to tag: \(error.localizedDescription)")
-                session.invalidate(errorMessage: "Connection error. Please try again.")
-                return
-            }
-            
-            switch tag {
-            case .iso15693(let iso15693Tag):
-                print("ISO15693 tag detected.")
-                session.invalidate()
-            default:
-                print("Unsupported tag type.")
-                session.invalidate(errorMessage: "Unsupported tag.")
-            }
-        }
-    }
-    
+class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "NFC App"
+        // Do any additional setup after loading the view.
     }
-    
-    @IBAction func scanTag(_ sender: Any) {
-        guard NFCNDEFReaderSession.readingAvailable else {
-            let alertController = UIAlertController(
-                title: "Scanning Not Supported",
-                message: "This device doesn't support tag scanning.",
-                preferredStyle: .alert
-            )
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alertController, animated: true, completion: nil)
-            return
-        }
-        
-        self.session = NFCTagReaderSession(pollingOption: [.iso15693], delegate: self, queue: nil)
-        self.session?.alertMessage = "Hold your iPhone near the tag."
-        self.session?.begin()
-    }
+
+
 }
 
